13b4bd160bd75ad95d775efba498830c54c62953
水管家管理员 <admin@zhsw.com>
2026-04-22 10:33:30 +0800

    fix: 在安全管理父页面添加RouterView以显示子路由内容

--- src/views/safety/index.vue
+++ src/views/safety/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="safety-page">
-    <div class="placeholder-content">
-      <p class="page-title">安全管理</p>
-      <p class="page-desc">页面开发中...</p>
-    </div>
+    <RouterView />
   </div>
 </template>
 
@@ -13,29 +10,6 @@
 
 <style lang="scss" scoped>
   .safety-page {
-    display: flex;
-    flex-direction: column;
-    height: calc(100vh - 140px);
-    overflow: hidden;
-    padding: 0;
-
-    .placeholder-content {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      color: #909399;
-
-      .page-title {
-        font-size: 24px;
-        font-weight: 500;
-        margin-bottom: 12px;
-      }
-
-      .page-desc {
-        font-size: 14px;
-      }
-    }
+    height: 100%;
   }
 </style>
